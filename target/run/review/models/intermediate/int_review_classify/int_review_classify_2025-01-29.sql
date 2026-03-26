851c8cc70009e6258941134d1e03eb1be187031e
Deydson Pontes <99624552+deydsonx@users.noreply.github.com>
2026-03-26 12:22:18 -0300

    Delete target directory

--- target/run/review/models/intermediate/int_review_classify/int_review_classify_2025-01-29.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `ecommerce`.`intermediate`.`int_review_classify`
- REPLACE WHERE cast(ts_review_answer as TIMESTAMP) >= '2025-01-29 00:00:00+00:00' and cast(ts_review_answer as TIMESTAMP) < '2025-01-30 00:00:00+00:00'
- TABLE `int_review_classify__dbt_tmp_20250129`
--- target/run/review/models/intermediate/int_review_classify/int_review_classify_2025-01-30.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `ecommerce`.`intermediate`.`int_review_classify`
- REPLACE WHERE cast(ts_review_answer as TIMESTAMP) >= '2025-01-30 00:00:00+00:00' and cast(ts_review_answer as TIMESTAMP) < '2025-01-31 00:00:00+00:00'
- TABLE `int_review_classify__dbt_tmp_20250130`
